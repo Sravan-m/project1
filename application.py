682864eb4ab172f0d5466a2cba4671c756c52410
Sravan-m <65647304+Sravan-m@users.noreply.github.com>
2020-12-30 19:13:43 +0530

    Fixed search bug

--- application.py
+++ application.py
@@ -139,8 +139,7 @@ def registration():
                 # return redirect(url_for('register'))
                 return redirect(url_for('register'))
         except SQLAlchemyError as e:
-            #print(e)
-            print(app.config["DATABASE_URL"])
+            print(e)
             return render_template('fail.html',path='./static/css/styles.min.css')
         finally:
             db.close()
@@ -220,8 +219,8 @@ def content(row):
     for r in row:
         print(type(r.isbn))
         html += '''<tr>
-          <td><a onclick="bookdetails('''+str(r.isbn)+''')">'''+str(r.isbn)+'''</a></td>
-          <td><a onclick="bookdetails('''+str(r.isbn)+''')">'''+str(r.title)+'''</a></td>
+          <td><a onclick="bookdetails('''+"'"+str(r.isbn)+"'"+''')">'''+str(r.isbn)+'''</a></td>
+          <td><a onclick="bookdetails('''+"'"+str(r.isbn)+"'"+''')">'''+str(r.title)+'''</a></td>
         </tr>'''
 
     html += '''</tbody>
@@ -248,9 +247,9 @@ def searchtest():
 def booksearch(isbn):
     if 'email' in session:
         print('length of isbn',len(isbn))
-        if len(isbn)!=10:
+        while len(isbn)<10:
             isbn = "0"+isbn
-            booksearch(isbn)
+            # booksearch(isbn)
         print("isbn = ",isbn)
         db = scoped_session(sessionmaker(bind=engine))
         html = ''
